f3930c85b2e4226298ceb8b57d7f2d9882c44158
flick-23 <venkigd8@gmail.com>
2020-03-08 11:43:31 +0530

    Added FileSystemModule

--- FileSystemModule.js
+++ FileSystemModule.js
@@ -1,3 +1,25 @@
-//Working wiht files in node 
+//Working with files in node 
 
 const fs = require('fs');   //importing fileSystem module
+
+//Every operation is in Synchronous or Blocking , or Asynchronous or non blocking 
+//Avoid using Sync methods - its used for simplicity
+//For real world applications use Async , cz these are non blocking 
+
+const files = fs.readdirSync('./'); //Sync form 
+console.log(files);
+
+//Async form of the above method
+
+// Observe the intellisense, declare a function to handle the output if there is error or not
+// fs.readdir('<path>', function(err, files){
+//  ..statements ..})    
+
+fs.readdir('./', function(err, files){
+    if(err) 
+        console.log('ERROR',err);
+    else
+        console.log("Result",files);
+})
+
+//For more examples read node docs - File System Module
