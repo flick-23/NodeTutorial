2c2c0772cd6bfd1a91048a183218a34c42929b76
flick-23 <venkigd8@gmail.com>
2020-03-10 21:43:33 +0530

    Starting Named Functions method

---
+++ Async-Demo/NamedFunctions.js
@@ -0,0 +1,4 @@
+//Named Functions help organise the Call Backs Hell (nested CallBacks) & makes code look cleaner
+// It's another approach of A-Sync Programming
+
+//rewriting code in callBack using Named Functions method
