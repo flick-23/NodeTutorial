dfb336fd5c3596e56a06eda6020f021f90f9bb15
flick-23 <venkigd8@gmail.com>
2020-03-11 00:24:46 +0530

    Working on Promises

--- Async-Demo/Promises.js
+++ Async-Demo/Promises.js
@@ -4,3 +4,22 @@
  *      If all good => Fullfiled
  *      else => error
  */
+
+
+ //resolve & reject are basically functions
+ const p = new Promise((resolve, reject) => {
+    //Some async work
+    
+    setTimeout(() =>{
+      resolve(1); //if everything is good
+
+      reject(new Error('message'));     //if there's a prob, throw the error, dont just display a message 
+    }, 2000);
+    
+ });
+
+ //Consuming the promise
+//catch - Used to catch any error
+//then - getting the result of our async operation
+ p.then( result => console.log('Result : ',result));
+ p.catch(err => console.log('Error! ',err.message));
