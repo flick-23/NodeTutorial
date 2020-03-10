c7d1140bc141b86ab0a2840ccf030dc2e247994d
flick-23 <venkigd8@gmail.com>
2020-03-10 23:34:31 +0530

    Completed Named Functions

--- Async-Demo/NamedFunctions.js
+++ Async-Demo/NamedFunctions.js
@@ -2,3 +2,56 @@
 // It's another approach of A-Sync Programming
 
 //rewriting code in callBack using Named Functions method
+
+//We will replace each anonymous function with named function to flatten the struture of code.
+
+
+
+//Replaced all callback functions with named functions
+getUser(1,displayUser);
+
+getRepository('flick_23',displayRepositories);
+       
+getCommits((displayCommits));   
+
+
+//Named functions with their respective operations
+function displayCommits(commits){
+    console.log(commits);
+}
+
+function displayRepositories(repos){
+    console.log('Repositories : ',repos);
+}
+
+function displayUser(user){
+    console.log("User : ",user);
+}
+
+
+//Functio logics
+function getUser(id, callback){
+    setTimeout(() =>{
+        console.log('\nReading a user from a database . . .');
+
+        //id, gitHubUsername are parameter values that are being initialized and returned
+        callback( {id: id, gitHubUsername: 'flick-23'} );
+    }, 2000);
+}
+
+
+
+function getRepository(username,callback){
+    setTimeout(() =>{
+        console.log('\nCalling GitHub API . . . ');
+        callback(['Repo 1', 'Repo 2', 'Repo 3', 'Repo 4']);
+    }, 2000);
+}
+
+
+function getCommits(callback){
+    setTimeout(() =>{
+        console.log('\nCalling GitHub API . . . ');
+        callback('23 commits');
+    }, 2000);
+}
