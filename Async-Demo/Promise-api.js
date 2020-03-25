afa6823a381b11e74a9e5294b39d6575148b355c
flick-23 <venkigd8@gmail.com>
2020-03-25 12:30:06 +0530

    Working on Parallel Promises

--- Async-Demo/Promise-api.js
+++ Async-Demo/Promise-api.js
@@ -7,3 +7,27 @@ p.then(result => console.log(result));
 //Its a good practice to use the Error object
 const q = Promise.reject(new Error('Reason for Rejection'));    
 q.catch(error => console.log(error));
+
+//Parallel Promises
+
+//First this async operation is executed
+const p1 = new Promise((resolve) =>{
+    setTimeout(() =>{
+        console.log('Aysnc operation 1 ..');
+        resolve(1);
+    }, 2000);
+});
+
+//then this is executed
+const p2 = new Promise((resolve) =>{
+    setTimeout(() =>{
+        console.log('Aysnc operation 2 ..');
+        resolve(2);
+    }, 2000);
+});
+
+//KickOff both these sync operations and do something when result of both is ready
+
+//returns a new promise, when all promises in this array are resolved
+Promise.all([p1,p2])
+.then(result =>console.log(result));
