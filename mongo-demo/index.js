985133cc564dcaad59c6f6d0931e5ea1c9b39e70
Venkatesh D <venkigd8@gmail.com>
2022-01-28 19:10:56 +0530

    Connecting and getting started with MongoDB

---
+++ mongo-demo/index.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+//connection string
+////this method creates a promise
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB : ", err));
