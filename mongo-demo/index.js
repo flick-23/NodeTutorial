2d6f867bd72e5342740af3f0979485d7611ccf8f
Venkatesh D <venkigd8@gmail.com>
2022-01-28 19:20:07 +0530

    Schemas

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,3 +1,4 @@
+const { boolean, string } = require("joi");
 const mongoose = require("mongoose");
 
 //connection string
@@ -6,3 +7,14 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB : ", err));
+
+//schemas => use schemas to define shape of docs within  a collection of mongodb
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now }, //we can set default values like this
+  isPublished: Boolean,
+  //   other datatypes that can be used are
+  //   string, number, date, buffer, boolean, objectID, array
+});
