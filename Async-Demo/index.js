f96fc6044a38dfaf91eaf8e83b0e75da2c4e75f9
Venkatesh D <venkigd8@gmail.com>
2022-01-28 18:04:43 +0530

    Async Await approach

--- Async-Demo/index.js
+++ Async-Demo/index.js
@@ -2,11 +2,22 @@ console.log("Before"); //sync - code
 // getUser(1, getRepositories);
 console.log("After"); //sync - code
 
+//promise approach
 getUser(1)
   .then((user) => getRepositories(user.gitHubUsername))
   .then((repos) => getCommits(repos[0]))
   .then((commits) => console.log("\nCommits : ", commits))
   .catch((err) => console.log("Error : ", err.message));
+
+//async await
+async function displayCommits() {
+  const user = await getUser(1);
+  const repos = await getRepositories(user.gitHubUsername);
+  const commits = await getCommits(repos[0]);
+  console.log(commits);
+}
+
+displayCommits();
 
 function getUser(id) {
   return new Promise((resolve, reject) => {
