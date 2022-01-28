0aa96e1aa7e9a4e02e602fdc304d17bb80602926
Venkatesh D <venkigd8@gmail.com>
2022-01-28 12:45:35 +0530

    callbacks

--- Async-Demo/index.js
+++ Async-Demo/index.js
@@ -1,12 +1,25 @@
 console.log("Before"); //sync - code
-const user = getUser(1);
-console.log(user);
+getUser(1, function (user) {
+  console.log("User : ", user);
+
+  getRepositories(user.gitHubUsername, (repos) => {
+    console.log("Repos : ", repos);
+  });
+});
+
 console.log("After"); //sync - code
 
-function getUser(id) {
+function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from the database ....");
-    return { id: id, gitHubUsername: "mosh" };
+    callback({ id: id, gitHubUsername: "mosh" });
   }, 2000);
   return 1;
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log("\nCalling github API");
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
 }
