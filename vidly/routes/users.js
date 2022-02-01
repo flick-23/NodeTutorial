bbfee08247f09d9cde5f10deee82adeba0db05be
Venkatesh D <venkigd8@gmail.com>
2022-02-01 17:41:30 +0530

    Using lodash

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,5 +1,6 @@
 const { validate, User } = require("../models/user");
 const express = require("express");
+const _ = require("lodash");
 const router = express.Router();
 
 //Create
@@ -10,15 +11,12 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
   await user.save();
 
-  res.send(user);
+  _.pick(user, ["name", "email"]);
+
+  res.send(_.pick(user, ["_id", "name", "email"]));
 });
 
 module.exports = router;
