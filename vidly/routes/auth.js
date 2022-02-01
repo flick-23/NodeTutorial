7d7c9325667cb59c7e090be54b1a2873246f0924
Venkatesh D <venkigd8@gmail.com>
2022-02-01 23:06:57 +0530

    Generating auth token

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -2,6 +2,7 @@ const { User } = require("../models/user");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const router = express.Router();
 
@@ -16,7 +17,9 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
 
-  res.send(true);
+  const token = jwt.sign({ _id: user._id }, "jwtPrivateKey");
+
+  res.send(token);
 });
 
 function validate(req) {
