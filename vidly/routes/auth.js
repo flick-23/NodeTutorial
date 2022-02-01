d2eab4c48fb00cf2b3e6eb12750b65e78cf09e5e
Venkatesh D <venkigd8@gmail.com>
2022-02-01 18:10:35 +0530

    Testing the auth

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -2,6 +2,7 @@ const { User } = require("../models/user");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const express = require("express");
+const Joi = require("joi");
 const router = express.Router();
 
 //Create
