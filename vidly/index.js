a2310c936dddd31bfc6b3c97c8b64679d9d64d41
Venkatesh D <venkigd8@gmail.com>
2022-02-03 01:27:57 +0530

    Refactoring index.js - extracting routes

--- vidly/index.js
+++ vidly/index.js
@@ -1,21 +1,13 @@
 require("express-async-errors");
 require("winston-mongodb");
-const auth = require("./routes/auth");
 const config = require("config");
-const customers = require("./routes/customers");
-const error = require("./middleware/error");
 const express = require("express");
-const genres = require("./routes/genres");
 const Joi = require("Joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
 const winston = require("winston");
-
 const app = express();
-
+require("./startup/routes")(app);
 //handle any uncaught exceptions that arent in any of the catch blocks
 
 winston.handleExceptions(
@@ -52,16 +44,6 @@ mongoose
   .connect("mongodb://localhost/vidly")
   .then(() => console.log("DB connected..."))
   .catch((er) => console.error("DB connection error! "));
-
-app.use(express.json());
-app.use("/api/auth", auth);
-app.use("/api/customers", customers);
-app.use("/api/genres", genres);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening onf port ${port}`));
---
+++ vidly/startup/routes.js
@@ -0,0 +1,19 @@
+const auth = require("..//routes/auth");
+const error = require("../middleware/error");
+const express = require("express");
+const customers = require("../routes/customers");
+const genres = require("../routes/genres");
+const movies = require("../routes/movies");
+const rentals = require("../routes/rentals");
+const users = require("../routes/users");
+
+module.exports = function (app) {
+  app.use(express.json());
+  app.use("/api/auth", auth);
+  app.use("/api/customers", customers);
+  app.use("/api/genres", genres);
+  app.use("/api/movies", movies);
+  app.use("/api/rentals", rentals);
+  app.use("/api/users", users);
+  app.use(error);
+};
