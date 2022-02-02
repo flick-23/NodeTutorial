2d698060b5bdc6ef95cb6afee66516f4163c5691
Venkatesh D <venkigd8@gmail.com>
2022-02-02 13:29:13 +0530

    Role based authorization

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,4 +1,5 @@
 const { validate, Genre } = require("../models/genre");
+const admin = require("../middleware/admin");
 const auth = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
@@ -47,7 +48,7 @@ router.put("/:id", async (req, res) => {
 });
 
 //DELETE
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndRemove(req.params.id);
 
   if (!genre) return res.status(404).send("Genre not found!");
